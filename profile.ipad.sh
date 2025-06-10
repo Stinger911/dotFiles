6e3f4d2a2d95ec21df738222017438de46ee7f20
Andrew Abramov <stinger911@gmail.com>
2025-06-10 22:11:23 +0200

    refactor to separate files

---
+++ profile.ipad.sh
@@ -0,0 +1,51 @@
+alias ll="ls -Al"
+alias vim="nvim"
+alias vi="nvim"
+alias ll='ls -FlAhp'                       # Preferred 'ls' implementation
+alias less='less -FSRX'                     # Preferred 'less' implementation
+cd() { builtin cd "$@"; ll; }               # Always list directory contents upon 'cd'
+alias cd..='cd ../'                         # Go back 1 directory level (for fast typers)
+alias ..='cd ../'                           # Go back 1 directory level
+alias ...='cd ../../'                       # Go back 2 directory levels
+alias .3='cd ../../../'                     # Go back 3 directory levels
+alias .4='cd ../../../../'                  # Go back 4 directory levels
+alias .5='cd ../../../../../'               # Go back 5 directory levels
+alias .6='cd ../../../../../../'            # Go back 6 directory levels
+alias ~="cd ~"                              # ~:            Go Home
+alias c='clear'                             # c:            Clear terminal display
+alias path='echo -e ${PATH//:/\\n}'         # path:         Echo all executable Paths
+mcd () { mkdir -p "$1" && cd "$1"; }        # mcd:          Makes new Dir and jumps inside
+alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
+
+#   extract:  Extract most know archives with one command
+#   ---------------------------------------------------------
+extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+alias myip='curl ip.appspot.com'                    # myip:         Public facing IP Address
+
+# export TERM_FONT=/root/fonts/0xProtoNerdFontMono-Bold.ttf
+export COLORTERM=truecolor
+export EDITOR=hx
+export PATH=$PATH:/root/.local/bin
+. /root/.cache/oh-my-posh/init.*.sh
+# load git aliases
+. ./pvt-tools/gitalias.sh
+
